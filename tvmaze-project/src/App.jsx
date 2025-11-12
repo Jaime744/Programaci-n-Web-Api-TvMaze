671812c213e553d813ea80d695e02e947db091dd
Jaime744 <Jaime.94cr@gmail.com>
2025-11-12 21:04:42 +0100

    Busqueda en la api y sale abajo la foto e info

--- tvmaze-project/src/App.jsx
+++ tvmaze-project/src/App.jsx
@@ -5,22 +5,34 @@ import './App.css'
 import NavBar from './navBar.jsx'
 
 function App() {
+    const [results, setResults] = useState([])        
+    const [selectedShow, setSelectedShow] = useState(null)
     const handleSearch = async (query) => {
     const apiRes = await fetch(`https://api.tvmaze.com/search/shows?q=${query}`);
-    if (!apiRes.ok) {
-      throw new Error('Failed to fetch data from TV Maze API');
-    }
-    const data = await apiRes.json();
-    console.log(data);
-    if(data.length > 0){
-      alert("Busqueda exitosa");
-    } else {
-      alert("No se encontraron resultados"); 
-    } 
-    }; 
+        if (!apiRes.ok) {
+        throw new Error('Error al buscar la api');
+        }
+        try{
+            const data = await apiRes.json();
+            console.log(data);
+            setResults(data.map(item => item.show));
+        }catch(error){
+            console.error('Error en respuesta de la API', error);
+        };
+    }  
+    const handleCloseModal = () => setSelectedShow(null);
   return (
     <div className="App">
       <NavBar onSearch={handleSearch}/>
+      <div className='results-container'>
+        {results.map(show => (
+            <div key={show.id} className='show-card'>
+                <h2>{show.name}</h2>
+                {show.image && <img src={show.image.medium} alt={show.name} />}
+                <p dangerouslySetInnerHTML={{__html: show.summary}}></p>
+            </div>
+        ))}
+      </div>
     </div>
   )
 }
