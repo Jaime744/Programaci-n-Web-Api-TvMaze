41c95c24e1f167d30873c2bbfd77e14ee6dbd1ec
Jaime744 <Jaime.94cr@gmail.com>
2025-11-12 20:14:20 +0100

    Nav bar principal muy simple aun no busca nada

---
+++ tvmaze-project/src/navBar.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import React from "react";
+import "./index.css";
+
+export default function NavBar({ onSearch }) {
+  const [query, setQuery] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (query.trim()) {
+      onSearch(query);
+      setQuery("");
+    }
+  };
+
+    return (
+        <nav className="nav-bar">
+            <div className="nav-BarContainer">
+                <h1 className="nav-barTitle">Tv Maze Finder</h1>
+                <form onSubmit={onSearch} className="nav-BarForm">
+                    <input type="text" placeholder="Buscar serie" value={query} onChange={(e)=> setQuery(e.target.value)} className="nav-BarInput"/>
+                    <button type="submit" className="nav-BarButton">Buscar</button>
+                </form>
+            </div>
+        </nav>
+    );
+}
